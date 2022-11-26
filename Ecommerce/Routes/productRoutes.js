9e69d79e533f98e81b943b5d9b6a8161fa65adae
JuanMaLopez2 <100231247+JuanMaLopez2@users.noreply.github.com>
2022-11-25 23:52:44 -0500

    Update productRoutes.js

--- Ecommerce/Routes/productRoutes.js
+++ Ecommerce/Routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const productsModel = require("../models/products");
+const productsModels = require("../models/products");
 
 
 
@@ -15,7 +15,7 @@ router.post('/insert', function (req, res, next) {
     if (!name || !price) {
         return res.status(500).send("No name or price");
     }
-    productsModel
+    productsModels
         .insertar(name, price)
         .then(idProductInsert => {
             res.redirect("/products");
@@ -30,7 +30,7 @@ router.post('/insert', function (req, res, next) {
 // Read all products
 // URI: /products/read
 router.get('/', function (req, res, next) {
-    productsModel
+    productsModels
         .obtain()
         .then(products => {
             res.render("products/read", {
@@ -52,7 +52,7 @@ router.post('/update/', function (req, res, next) {
     if (!name || !price || !id) {
         return res.status(500).send("Not enough data");
     }
-    productsModel
+    productsModels
         .update(id, name, price)
         .then(() => {
             res.redirect("/products");
@@ -67,7 +67,7 @@ router.post('/update/', function (req, res, next) {
 //Delete
 // URI: /products/delete/{id}
 router.get('/delete/:id', function (req, res, next) {
-    productsModel
+    productsModels
         .delete(req.params.id)
         .then(() => {
             res.redirect("/products");
@@ -82,7 +82,7 @@ router.get('/delete/:id', function (req, res, next) {
 //Consult
 // URI: /products/consult/{id}
 router.get('/consult/:id', function (req, res, next) {
-    productsModel
+    productsModels
         .obtainById(req.params.id)
         .then(product => {
             if (product) {
@@ -98,6 +98,102 @@ router.get('/consult/:id', function (req, res, next) {
         });
 });
 
+//operaciones Carrito
+//------------------------------------------------------------------------------------
 
-
+// http://localhost:3000/Kart/Createkart/:CustomerId
+router.post("/Kart/CreateKart/:CustomerId", function (req, res, next) {
+    const { confirmation } = req.body;
+    if (!confirmation) {
+      return res.status(500).send("No confirmaste la creacion del carrito");
+    } else {
+      productsModels.createKart(req.params.CustomerId, confirmation);
+      return res.send({ message: `Carrito Creado!` });
+    }
+  });
+  
+  
+  // http://localhost:3000/Kart/ProductInsert/:CustomerKartId
+  router.post("/Kart/ProductInsert/:CustomerKartId", function (req, res, next) {
+    const { Product, Quantity } = req.body;
+    if (!Product || !Quantity) {
+      return res.status(500).send("No agregaste el producto o la cantidad");
+    } else {
+      productsModels.ProductInsert(req.params.CustomerKartId, Product, Quantity);
+      return res.send({ message: `Producto Agregado!` });
+    }
+  });
+  
+  
+  //http://localhost:3000/Kart/UpdateQuantity/:CustomerKartId
+  router.put("/Kart/UpdateQuantity/:CustomerKartId", function (req, res, next) {
+    const { Quantity, ProductId } = req.body;
+    productsModels
+      .UpdateQuantity(Quantity, ProductId, req.params.CustomerKartId)
+      .then(() => {
+        res.send({ message: `Cantidad Actualizada!!` });
+      })
+      .catch((err) => {
+        return res.status(500).send(err);
+      });
+  });
+  
+  
+  //http://localhost:3000/Kart/DeleteProduct/:CustomerKartId 
+  router.delete("/Kart/DeleteProduct/:CustomerKartId", function (req, res, next) {
+    const { ProductId } = req.body;
+    productsModels
+      .DeleteProduct(ProductId, req.params.CustomerKartId)
+      .then(() => {
+        res.send({ message: `Producto Eliminado!` });
+      })
+      .catch((err) => {
+        return res.status(500).send("Error eliminando");
+      });
+  });
+  
+  
+  //http://localhost:3000/Kart/MakePurchase1/:CustomerKartId
+  router.post("/Kart/MakePurchase1/:CustomerKartId", function (req, res, next) {
+    const { CustomerId, direction_delivery, mail_order } = req.body;
+    if (!CustomerId || !direction_delivery || !mail_order) {
+      return res.status(500).send("No agregaste algun campo requerido");
+    } else {
+      productsModels.MakePurchase1(CustomerId, direction_delivery, mail_order);
+      productsModels.MakePurchase2(req.params.CustomerKartId);
+      return res.send({ message: `Operacion exitosa!!` });
+    }
+  });
+  
+  
+  // http://localhost:3000/Kart/ConsultKart/:CustomerKartId
+  router.get("/Kart/consultKart/:CustomerKartId", async (req, res) => {
+    try {
+      const products = await productsModels.GetKart(req.params.CustomerKartId);
+      res.send(products);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
+  
+  
+  //http://localhost:3000/Kart/ConsultOrder/:CustomerId
+  router.get("/Kart/ConsultOrder/:CustomerId", async (req, res) => {
+    try {
+      const products = await productsModels.GetOrder(req.params.CustomerId);
+      res.send(products);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
+  
+  //http://localhost:3000/Kart/GetCustomerKart
+  router.get("/Kart/GetCustomerKart", async (req, res) => {
+    try {
+      const results = await productsModels.GetCustomerKart();
+      res.send(results);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+});
 module.exports = router;
