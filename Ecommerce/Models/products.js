a58cb25e6049adf9077d2db473ac2d3098e66f2c
JuanMaLopez2 <100231247+JuanMaLopez2@users.noreply.github.com>
2022-11-26 00:16:02 -0500

    Update products.js

--- Ecommerce/Models/products.js
+++ Ecommerce/Models/products.js
@@ -53,4 +53,102 @@ module.exports = {
                 });
         });
     },
+    
+    
+//Crear un nuevo carrito 
+CreateKart(CustomerId, confirmation) {
+  return new Promise((resolve, reject) => {
+      conexion.query(`insert into CustomerKart(CustomerId,confirmation, estatus) values(?,?, "Purchasing") `,
+          [CustomerId, confirmation], (err, results) => {
+              if (err) reject(err);
+              else resolve(results.insertId);
+          });
+  });
+},
+
+//agregar un producto al carrito
+ProductInsert(CustomerId, ProductId, Quantity) {
+  return new Promise((resolve, reject) => {
+      conexion.query(`insert into carrito(CustomerKartId, ProductId, Quntity) values(?,?,?) `,
+          [CustomerKartId, ProductId, Quantity], (err, results) => {
+              if (err) reject(err);
+              else resolve(results.insertId);
+          });
+  });
+},
+
+// actualizar cantidad de un producto en el carrito
+UpdateQuantity(Quantity, ProductId, CustomerKartId) {
+  return new Promise((resolve, reject) => {
+      conexion.query(`update ecommerce.Kart
+                      set Quantity = ?
+                      where ProductId= ? and CustomerKartId = ?`,
+          [Quantity, ProductId, CustomerKartId],
+          (err) => {
+              if (err) reject(err);
+              else resolve();
+          });
+  });
+},
+
+// eliminar un producto del carrito
+DeleteProduct(ProductId, CustomerKartId) {
+  return new Promise((resolve, reject) => {
+      conexion.query(`delete from ecommerce.Kart
+  where  ProductId = ? and CustomerKartId= ?`,
+          [ProductId, CustomerKartId],
+          (err) => {
+              if (err) reject(err);
+              else resolve();
+          });
+  });
+},
+
+// primera parte para comprar/ordenar un carrito
+MakePurchase1(CustomerId, direction_delivery, mail_order) {
+  return new Promise((resolve, reject) => {
+      conexion.query(`insert into orden (CustomerId, direction_delivery, mail_order, date_order, status_order) values (?,?,?, NOW(), "aceptada")`,
+          [CustomerId, direction_delivery, mail_order],
+          (err) => {
+              if (err) reject(err);
+              else resolve();
+          });
+  });
+},
+
+
+// consulta el carrito
+ConsultKart(CustomerKartId) {
+  return new Promise((resolve, reject) => {
+      conexion.query(`select  KartID , ProductId, Quantity from Kart where CustomerKartId = ?`,
+          [CustomerKartId],
+          (err, results) => {
+              if (err) reject(err);
+              else resolve(results);
+          });
+  });
+},
+
+// consulta para hacer pruebas sobre una orden
+ConsultOrder(CustomerId) {
+  return new Promise((resolve, reject) => {
+      conexion.query(`select * from order where CustomerId = ?`,
+          [CustomerId],
+          (err, results) => {
+              if (err) reject(err);
+              else resolve(results);
+          });
+  });
+},
+
+
+GetCustomerKart() {
+  return new Promise((resolve, reject) => {
+      conexion.query(`select * from CustomerKart`,
+          (err, results) => {
+              if (err) reject(err);
+              else resolve(results);
+          });
+  });
+},
 }
